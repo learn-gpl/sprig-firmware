cd1bd232d93c9590b7194597b3aea3084ad1db27
retrooper <retrooperdev@gmail.com>
2024-09-04 22:16:14 +0000

    attempt to color both LEDs of same color.

--- firmware/spade/src/rpi/main.c
+++ firmware/spade/src/rpi/main.c
@@ -148,14 +148,17 @@ static void power_lights() {
   gpio_set_function(pin_num_0, GPIO_FUNC_PWM);
   uint slice_num_0 = pwm_gpio_to_slice_num(pin_num_0);
   pwm_set_enabled(slice_num_0, true);
-  pwm_set_gpio_level(pin_num_0, 65535/8);
+
+	const int led_color = 3093151;
+
+  pwm_set_gpio_level(pin_num_0, led_color);
 
   // right blue light
    const pin_num_1 = 4;
    gpio_set_function(pin_num_1, GPIO_FUNC_PWM);
    uint slice_num_1 = pwm_gpio_to_slice_num(pin_num_1);
    pwm_set_enabled(slice_num_1, true);
-   pwm_set_gpio_level(pin_num_1, 65535/4);
+   pwm_set_gpio_level(pin_num_1, led_color);
 }
 
 // Entry point for the second core that polls the buttons.
